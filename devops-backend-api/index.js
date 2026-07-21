d8b85882824bbecc59cf4ed8afdc4cd10d01e402
codesbyshahzaib <shahzaib@camp2.tkxel.com>
2026-07-21 12:21:15 +0500

    testing pipleine 11

--- devops-backend-api/index.js
+++ devops-backend-api/index.js
@@ -4,7 +4,10 @@ const PORT = process.env.PORT || 3000;
 
 // This is the new root route! It handles requests to exactly "http://localhost:3000"
 app.get('/', (req, res) => {
-    res.send('<h1>Hello from my Docker Container! 🐳</h1><p>My DevOps Assignment is running successfully.</p>');
+    res.send('<h1>Hello from my Docker Container! 🐳</h1><p>My DevOps Assignment is running successfully .</p>');
+
+        res.send('<h1>Test 1</h1>');
+
 });
 
 // Your existing health check endpoint
