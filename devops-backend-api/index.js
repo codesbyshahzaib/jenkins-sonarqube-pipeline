61cb93b8ae12ce4e7550b17af1e0aaed645f9e97
codesbyshahzaib <shahzaib@camp2.tkxel.com>
2026-07-22 11:54:03 +0500

    improved docker 43

--- devops-backend-api/index.js
+++ devops-backend-api/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('<h1>Hello from my Docknkxer Container! 🐳</h1><p>My DevOps Assignment is running successfully .</p>');
 
-    res.send('<h1>Test 11211</h1>');
+    res.send('<h1>Testing hte pipeline </h1>');
 });
 
 // Your existing health check endpoint
