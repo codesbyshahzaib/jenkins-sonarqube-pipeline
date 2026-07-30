972d253c8484d89a2469e68feee9dc4cdb87fde7
codesbyshahzaib <shahzaib@camp2.tkxel.com>
2026-07-30 15:43:58 +0500

    test2

--- devops-backend-api/index.js
+++ devops-backend-api/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 // This is the new root route! It handles requests to exactly "http://localhost:3000"
 app.get('/', (req, res) => {
-    res.send('<h1>Hello from my Docknkxer Container! 🐳</h1><p>My DevOps Assignment is running successfully .</p>');
+    res.send('<h1>Hello from test my Docknkxer Container! 🐳</h1><p>My DevOps Assignment is running successfully .</p>');
 
     res.send('<h1>Testing hte pipeline NO 1 </h1>');
 });
